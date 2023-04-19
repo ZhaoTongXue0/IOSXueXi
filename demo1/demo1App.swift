cb461d0cb852ffe8607d70acafc253f3a283a368
赵同学 <zhaoqianyu0916@163.com>
2023-04-19 16:33:30 +0800

    Initial Commit

---
+++ demo1/demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  demo1App.swift
+//  demo1
+//
+//  Created by 赵同学 on 2023/4/19.
+//
+
+import SwiftUI
+
+@main
+struct demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
